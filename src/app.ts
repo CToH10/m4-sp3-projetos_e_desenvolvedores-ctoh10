2f717de8e716526725cace59de6185699820ce77
Luís Nunes <luis.ebn98@gmail.com>
2023-02-07 10:18:58 -0300

    feat: add db connection

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application } from "express";
 import { checkDeveloperKeys } from "../src/middlewares/post.middleware";
+import { startDatabase } from "../src/database/index";
 
 const app: Application = express();
 
@@ -9,6 +10,8 @@ app.post("/developers", checkDeveloperKeys);
 
 const port: number = 3000;
 const message: string = `Server is running on: http://localhost:${port}`;
+
 app.listen(port, async () => {
+  await startDatabase();
   console.log(message);
 });
